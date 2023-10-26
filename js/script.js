17877a2e2a4bd4072e50cd36a4c37fe5234ebf9e
Stasiuk <oldgreenfox@gmail.com>
2023-10-26 22:07:17 +0300

    Practic 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log(1);
+'use strict';
+const numberOfFilms = Number(prompt('Скільки фільмів ви уже подивились?'));
+const personalMovieDB = {
+	count : numberOfFilms,
+	movies : {},
+	actors : {},
+	genres : [],
+	private : false
+};
+const film1 = prompt('Один з останніх переглянутих фільмів?');
+const vote1 = prompt('Оцініть його');
+const film2 = prompt('Один з останніх переглянутих фільмів?');
+const vote2 = prompt('Оцініть його');
+
+personalMovieDB.movies[film1] = vote1;
+personalMovieDB.movies[film2] = vote2;
+console.log(personalMovieDB);
